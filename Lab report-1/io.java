51bd2bc004a246a7881e035910d29521d39fa59b
AI-Siyam <siyammd007@gmail.com>
2026-04-30 17:07:29 +0600

    Create io.java

---
+++ Lab report-1/io.java
@@ -0,0 +1,8 @@
+public class io {
+    public static void main(String[] args) 
+    {
+        System.out.println("Hi, I'm Siyam");
+        System.out.println("I'm 20 years old.");
+        System.out.println("Learning Java is fun!");
+    }
+}
