5fd4e26d57400ba5650b06fc82e2e7687ab6b89d
AI-Siyam <siyammd007@gmail.com>
2026-04-30 17:22:16 +0600

    Create string.java

---
+++ Lab report-1/string.java
@@ -0,0 +1,9 @@
+public class string {
+    public static void main(String[] args) {
+        String s = "AMINUL IslAm siYAm";
+
+        System.out.println(s.toUpperCase());
+        System.out.println(s.toLowerCase());
+        System.out.println(s.length());
+    }
+}
