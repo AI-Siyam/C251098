a5258a6c951a4290231ddc9a83ca05ea2867adb2
AI-Siyam <siyammd007@gmail.com>
2026-04-30 17:19:52 +0600

    Create operator.java

---
+++ Lab report-1/operator.java
@@ -0,0 +1,10 @@
+public class operators {
+    public static void main(String[] args) {
+        int sum = 10+12;
+        int multi = 10*30;
+        double div = 30/10;
+        int sub = 40-60;
+
+        System.out.println(sum + " " + multi + " " + div + " " + sub);
+    }
+}
