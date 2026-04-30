259ddee3ba1c5207c31d18492843dab2df2708df
AI-Siyam <siyammd007@gmail.com>
2026-04-30 17:15:45 +0600

    Create variable.java

---
+++ Lab report-1/variable.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class variable 
+{
+    public static void main(String[] arg)
+    {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        
+        System.out.println("your input is : " + n);
+
+        sc.close();
+    }
+}
