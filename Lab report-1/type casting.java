62bdbdc38605dfee5ac8698d31a4f2b57d3f26cf
AI-Siyam <siyammd007@gmail.com>
2026-04-30 17:18:59 +0600

    Create type casting.java

---
+++ Lab report-1/type casting.java
@@ -0,0 +1,8 @@
+public class typecasting {
+    public static void main(String[] args) {
+        double d = 9.78d; 
+
+        System.out.println(d); 
+        System.out.println((int)d);    
+    }
+}
