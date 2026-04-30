1a8f71d53b2efbe80fb94157b293198a0da8a05a
AI-Siyam <siyammd007@gmail.com>
2026-04-30 17:22:42 +0600

    Create math.java

---
+++ Lab report-1/math.java
@@ -0,0 +1,8 @@
+public class math {
+    public static void main(String[] args) {
+        int mx = Math.max(12,90);
+        int mn = Math.min(123, 456);
+
+        System.out.println(mx + " " + mn);
+    }
+}
