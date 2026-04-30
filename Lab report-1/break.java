6e3a8b46e084c7cf37542c40b8f48e88c7e91efa
AI-Siyam <siyammd007@gmail.com>
2026-04-30 17:29:21 +0600

    Create break.java

---
+++ Lab report-1/break.java
@@ -0,0 +1,12 @@
+public class break_continue {
+    public static void main(String[] args) {
+        for (int i = 0; i < 6; i++) 
+        {
+            if (i == 2) continue;
+            
+            if (i == 4) break;
+            
+            System.out.println(i); 
+        }
+    }
+}
